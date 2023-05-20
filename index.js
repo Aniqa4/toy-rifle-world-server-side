f76abcbcdecbbd2022d7ac3b7c01dd036268a0f1
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-05-21 05:07:55 +0600

    completed delete operation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 //middleware
@@ -58,6 +58,7 @@ async function run() {
       res.send(result);
     })
 
+
     //get all data
     app.get('/myToys', async (req, res) => {
       const cursor = toyCollection.find();
@@ -65,11 +66,29 @@ async function run() {
       res.send(result);
     })
 
+    //get a single toy by specific id
+    app.get('/myToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    })
 
-    //add new documents to the collection
+
+    /* //add new documents to the collection
     app.post('/addToys', async (req, res) => {
       const newToy = req.body;
       const result = await toyCollection.insertOne(newToy);
+      res.send(result);
+    }) */
+
+
+    //delete data from collection
+    app.delete('/myToys/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log('id');
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
       res.send(result);
     })
 
